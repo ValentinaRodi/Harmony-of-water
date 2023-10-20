eb8f0a4b5cd27aa57b4c5d17accd206230fe0461
Родионова Валентина <rodionova.v@x3group.ru>
2023-10-20 22:19:40 +0500

    Add site

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
-const carousel = document.querySelector('.carousel');
-const slides = carousel.querySelector('.slides');
-const prevButton = carousel.querySelector('#prevButton');
-const nextButton = carousel.querySelector('#nextButton');
-const windowWidth = window.screen.width;
+const carousel = document.querySelector(".carousel");
+const slides = carousel.querySelector(".slides");
+const prevButton = carousel.querySelector("#prevButton");
+const nextButton = carousel.querySelector("#nextButton");
 
 let slideIndex = 0;
 
-prevButton.addEventListener('click', () => {
+prevButton.addEventListener("click", () => {
   slideIndex--;
   if (slideIndex < 0) {
-    slideIndex = slides.children.length - 1;
+    slideIndex = 0;
   }
   updateSlidePosition();
 });
 
-nextButton.addEventListener('click', () => {
+nextButton.addEventListener("click", () => {
   slideIndex++;
-  if (slideIndex >= slides.children.length) {
+
+  if (
+    slides.offsetWidth >
+    (slides.children.length - slideIndex) * (305 + 22) + 300
+  ) {
     slideIndex = 0;
   }
   updateSlidePosition();
 });
 
 function updateSlidePosition() {
-  let slidePosition;
-  (windowWidth > 375) ? slidePosition = -slideIndex * (305+22): slidePosition = -slideIndex * 300;
+  let slidePosition = -slideIndex * (305 + 22);
   slides.style.transform = `translateX(${slidePosition}px)`;
-  console.log(slidePosition)
-  console.log(slideIndex)
-  console.log(slideWidth)
 }
 
+// Touch slider
+let slideCount = slides.children.length;
+let startX = 0;
+let endX = 0;
+let isDragging = false;
+const windowWidht = window.screen.width;
 
+function touchStart() {
+  return function (event) {
+    startX = event.touches[0].clientX;
+    isDragging = true;
+  };
+}
+
+function touchMove(event) {
+  if (isDragging) {
+    endX = event.touches[0].clientX;
+  }
+}
+
+function touchEnd() {
+  isDragging = false;
+  let movedBy = startX - endX;
+
+  if (movedBy > 0 && slideIndex < slideCount - 1) {
+    slideIndex++;
+  }
+
+  if (movedBy < 100 && slideIndex > 0) {
+    slideIndex--;
+  }
+
+  updateSlidePosition();
+}
+
+if (windowWidht <= 768) {
+  slides.addEventListener("touchstart", touchStart(0));
+  slides.addEventListener("touchmove", touchMove);
+  slides.addEventListener("touchend", touchEnd);
+}
